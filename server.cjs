a996e93e8cd73acbe5feb91ab27c019adfed6d3c
Davydenkov Ivan <118854180+IvanDavydenkov@users.noreply.github.com>
2024-03-09 22:56:56 +0300

    [правки]

--- server.cjs
+++ server.cjs
@@ -7,9 +7,9 @@ const PORT = 3000
 
 // Добавил чтобы работал кастомный роутер
 app.get('*', (req, res) => {
-    // res.sendFile(join(__dirname, 'dist', 'index.html'))
-    res.status(200).sendStatus(200).sendFile(join(__dirname, ''));
-})
+    res.sendFile(join(__dirname, 'dist', 'index.html'));
+});
+
 
 app.use(express.static('./dist'))
 
