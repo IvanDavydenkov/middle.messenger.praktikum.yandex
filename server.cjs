acb3a1499647951a9440ce520e899a805f18cbc1
Davydenkov Ivan <118854180+IvanDavydenkov@users.noreply.github.com>
2024-03-09 23:02:29 +0300

    [правки]

--- server.cjs
+++ server.cjs
@@ -5,15 +5,15 @@ const app = express()
 const PORT = 3000
 
 app.use(express.static('./dist'))
-
-// Добавил чтобы работал кастомный роутер
-app.get('*', (req, res) => {
-    res.sendFile(join(__dirname, 'dist', 'index.html'));
-});
 app.get('/', (req, res) => {
     res.status(200)
     res.send('Hello World!');
 });
+// Добавил чтобы работал кастомный роутер
+app.get('*', (req, res) => {
+    res.sendFile(join(__dirname, 'dist', 'index.html'));
+});
+
 
 app.listen(PORT, function () {
     console.log(`Server started on port ${PORT}!`)
