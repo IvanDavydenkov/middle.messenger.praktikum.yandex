ca46b88d7e3910931863216375801f606dc1404f
Davydenkov Ivan <118854180+IvanDavydenkov@users.noreply.github.com>
2024-03-09 22:40:29 +0300

    [правки]

--- server.cjs
+++ server.cjs
@@ -4,11 +4,11 @@ const {join} = require('path')
 const app = express()
 const PORT = 3000
 
-app.use(express.static('./dist/index.html'))
+app.use(express.static('./dist'))
 
 // Добавил чтобы работал кастомный роутер
 app.get('*', (req, res) => {
-    res.sendFile(join(__dirname, 'dist', 'index.html'))
+    res.status(200).sendFile(join(__dirname, 'dist', 'index.html'))
 })
 app.listen(PORT, function () {
     console.log(`Server started on port ${PORT}!`)
