93b9d0b46adf98d21ee1c4a125e4ce23258816ef
Davydenkov Ivan <118854180+IvanDavydenkov@users.noreply.github.com>
2024-03-10 20:17:38 +0300

    [правки]

--- server.cjs
+++ server.cjs
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 3000
 
 app.use(express.static('./dist'))
 app.get('/', (req, res) => {
+    res.sendFile(join(__dirname, "dist", "index.html"));
     res.status(200)
 })
 
