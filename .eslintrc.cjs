c88b3f72d773579244b80018a48bb17c366c1b1f
Davydenkov Ivan <118854180+IvanDavydenkov@users.noreply.github.com>
2024-04-27 02:43:00 +0300

    [правки]

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,29 +1,29 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+      rules: {
+        'no-unused-vars': 'off', // Отключаем правило no-unused-vars только для TypeScript файлов
+        '@typescript-eslint/no-unused-vars': 'off', // Отключаем аналогичное правило для TypeScript
+      },
     },
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-    overrides: [
-        {
-            env: {
-                node: true,
-            },
-            files: ['.eslintrc.{js,cjs}'],
-            parserOptions: {
-                sourceType: 'script',
-            },
-            rules: {
-                'no-unused-vars': 'off', // Отключаем правило no-unused-vars только для TypeScript файлов
-                '@typescript-eslint/no-unused-vars': 'off', // Отключаем аналогичное правило для TypeScript
-            },
-        },
-    ],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-    },
-    plugins: ['@typescript-eslint'],
-    rules: {},
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {},
 }
